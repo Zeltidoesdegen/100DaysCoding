fb4f995c19d11a5b30df61f9b61e9e799adbc9c6
mcarsula <ivan.carsula@gmail.com>
2025-03-19 17:44:41 +0800

    Variables

---
+++ Day1/Variables.py
@@ -0,0 +1,8 @@
+# Write your code below this line 👇
+# Single line code
+print(len(input("What is your name? ")))
+
+# Cleaner code
+userName = input("What is your name? ")
+length = len(userName)
+print(length)
