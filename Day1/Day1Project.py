259400979b19cea609fa1f181cf6a04b71a61eb1
mcarsula <ivan.carsula@gmail.com>
2025-03-19 18:49:55 +0800

    Day 1 Project - Errors: Not using \n and printing outcome of band_firstName and band_SecondName

---
+++ Day1/Day1Project.py
@@ -0,0 +1,6 @@
+print("Welcome to the Band Name Generator")
+
+band_firstName = input("What's the name of the city you grew up in? \n")
+band_secondName = input("What's your pet name? \n")
+
+print("Your band name could be " + band_firstName + " " + band_secondName )
